ad8c354ab283578b9ddd7fcc94f1810f55030ae8
Xian Andrei Villacampa <77653877+XAVillacampa@users.noreply.github.com>
2025-03-05 12:21:12 +0800

    Added run.js for testing

---
+++ run.js
@@ -0,0 +1,16 @@
+const concurrently = require("../../../AppData/Roaming/npm/node_modules/concurrently");
+const path = require("path");
+
+const frontendPath = path.resolve(__dirname, "frontend");
+const backendPath = path.resolve(__dirname, "backend");
+
+concurrently([
+  {
+    name: "frontend",
+    command: `cd ${frontendPath} && npm run dev`,
+  },
+  {
+    name: "backend",
+    command: `cd ${backendPath} && npm run dev`,
+  },
+]);
